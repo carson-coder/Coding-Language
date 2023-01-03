7368f58c7d89662e4b361fcb3b8322a1a810060a
carson-coder <carsondpool@gmail.com>
2023-01-03 16:45:07 -0500

    add command line args, init() function to parse code and define PRINT_ERR and EXIT_ON_ERR

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import io
 import re
+import sys
 
+
+# Args:
+# -debug: run in debug mode. Lets you define breakpoints. See var values.
+
+# -dev: debug compiler. dont put in docs
+
+global DEBUG
+global Error
+global ErrorMsg
+global DEV
 Error = False
 ErrorMsg = "You shouldn't see this"
+DEBUG = False
+DEV = False
+EXIT_ON_ERR = True
+PRINT_ERR = True
+
+class Code_Not_Loaded(Exception):
+    pass
+
+if "-debug" in sys.argv:
+    DEBUG = True
+if "-dev" in sys.argv:
+    DEV = True
 
 class Buffer():
     buffer = []
     def add(self, cmd):
         self.buffer.append(cmd)
     def remove(self, cmd):
+        global Error
+        global ErrorMsg
         try:
             self.buffer.remove(cmd)
         except ValueError:
@@ -36,4 +61,25 @@ class Compiler():
         #Code
         else:
             self.code = code
-        
+        self.code_loaded = True
+    def init(self):
+        global EXIT_ON_ERR
+        global PRINT_ERR
+        if self.code_loaded != True:
+            raise Code_Not_Loaded("Run load_code() before init()")
+        self.parsed_code = self.code.split("\n")
+        try:
+            if self.parsed_code.index("EXIT_ON_ERR is false") < 2:
+                EXIT_ON_ERR = False
+                if self.parsed_code.index("PRINT_ERR is false") < 2:
+                    PRINT_ERR = False
+        except ValueError:
+            pass
+        self.ready = True
+
+if DEV:
+    a = Compiler()
+    a.load_code("EXIT_ON_ERR is false")
+    a.init()
+
+    print(EXIT_ON_ERR, PRINT_ERR, DEBUG, a.code, a.parsed_code, a.ready, a.code_loaded)
